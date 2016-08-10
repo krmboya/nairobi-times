f80dfffe42284c7909165ad0782b869a7261ba42
Kenny Rachuonyo <krmboya@gmail.com>
2016-08-10 09:20:36 +0300

    fetch feed via proxy

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 /* Code for the site */
 $(function () {
+    
     var NATION_FEED_URL = "http://www.nation.co.ke/latestrss.rss";
+    
+    $.ajax({
+	url: "http://proxy.99nth.com",
+	headers: { "Upstream-URL": NATION_FEED_URL },
+	method: "GET",
+	success: function( data ) {
+	    $(data).find('item').each(function () {
+		var el = $(this);
+		console.log(el.find("title").text());
+	    });
+	}
+    });
+
+    console.log("request made");
+
 });
