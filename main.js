aa6c241fb6942785f67baf9aae0fdfe42c5f4620
Kenny Rachuonyo <krmboya@gmail.com>
2016-08-12 09:19:29 +0300

    add some helper functions

--- main.js
+++ main.js
@@ -2,19 +2,41 @@
 $(function () {
     
     var NATION_FEED_URL = "http://www.nation.co.ke/latestrss.rss";
-    
-    $.ajax({
-	url: "http://proxy.99nth.com",
-	headers: { "Upstream-URL": NATION_FEED_URL },
-	method: "GET",
-	success: function( data ) {
-	    $(data).find('item').each(function () {
-		var el = $(this);
-		console.log(el.find("title").text());
-	    });
-	}
-    });
+    var PROXY_URL = "http://proxy.99nth.com";
 
-    console.log("request made");
+    var fetchURL = function ( url ) {
+	// Triggers a GET request on the url.
+	// Returns a jqXHR object
+
+	var jqXHR = $.ajax({
+	    url: PROXY_URL,
+	    headers: { "Upstream-URL": url },
+	    method: "GET",
+	});
+
+	return jqXHR;
+    };
+
+    var parseRSS = function( xml ) {
+	// Parses rss xml returning a list of objects representing each entry
+
+	var items = [];
+
+	$(xml).find("item").each(function () {
+	    var element = $(this);
+	    var item = {
+		"title": element.find("title").text(),
+		"link": element.find("link").text(),
+		"description": element.find("description"),
+	    }
+
+	    items.push(item);
+	});
+
+	return items;
+    };
+    
+    var p = fetchURL(NATION_FEED_URL);
+    console.log(p);
 
 });
